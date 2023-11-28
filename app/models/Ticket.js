c8d29456c16a2907bd96f0829a0056e7914959a7
nguyentranthevu <nguyentranthevu@gmail.com>
2023-11-28 15:06:58 +0700

    mini nextjs  project

---
+++ app/models/Ticket.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+mongoose.connect(
+  "mongodb+srv://nguyentranthevu:nguyentranthevu300701@cluster0.5urkait.mongodb.net/?retryWrites=true&w=majority"
+);
+mongoose.Promise = global.Promise;
+
+const ticketSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    category: String,
+    priority: Number,
+    progress: Number,
+    status: String,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Ticket = mongoose.models.Ticket || mongoose.model("Ticket", ticketSchema);
+
+export default Ticket;
